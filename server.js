e32de6c12b65854b91dab7db342d626313953a26
LOubouzar <LOubouzar@gmail.com>
2018-10-29 20:58:52 -0400

    Heroku deployment

--- server.js
+++ server.js
@@ -10,17 +10,23 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //Node/Server Set up Test for Port 3001
-app.get('/', (req, res) => {
-  res.send(`Node Server Connected on PORT ${PORT}!`);
-});
+// app.get('*', (req, res) => {
+//   res.send(`Node Server Connected on PORT ${PORT}!`);
+// });
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
+
 // Add routes, both API and view
 app.use(routes);
 
